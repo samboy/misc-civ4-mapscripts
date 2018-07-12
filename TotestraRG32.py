91cb43edea972f71e0ecb6ded365ecc20067c78c
Sam Trenholme <strenholme.usenet@gmail.com>
2018-07-11 21:37:03 -0700

    RG32: Renumber maps, only T5 is insteresting to me

--- TotestraRG32.py
+++ TotestraRG32.py
@@ -858,7 +858,7 @@ class MapConstants :
         mapRString = "Random"
         self.totestra = 0 
         if selectionID == 1: # Totestra
-            self.totestra = 1 # Fixed map seed
+            self.totestra = 5 # Fixed map seed
             #mapRstring = "Totestra"
             # Ignore most map parameters
             #self.wrapX = True
@@ -869,7 +869,7 @@ class MapConstants :
             #heightmap_size_factor = 5
             #self.AllowPangeas = False
         elif selectionID == 2: # Cephalo
-            self.totestra = 2 # Fixed map seed
+            self.totestra = 6 # Fixed map seed
             #mapRstring = "Cephalo"
             # Ignore most map parameters
             #self.wrapX = True
@@ -880,7 +880,7 @@ class MapConstants :
             #heightmap_size_factor = 3
             #self.AllowPangeas = False
         elif selectionID == 3: # Caulixtla
-            self.totestra = 3 # Fixed map seed
+            self.totestra = 7 # Fixed map seed
             #mapRstring = "Caulixtla"
             # Ignore most map parameters
             #self.wrapX = True
@@ -891,9 +891,9 @@ class MapConstants :
             #heightmap_size_factor = 3
             #self.AllowPangeas = False
 	elif selectionID == 4: # En Dotter 1
-            self.totestra = 4 # En Dotter's low on resources map 
+            self.totestra = 8 # En Dotter's low on resources map 
 	elif selectionID == 5:
-	    self.totestra = 5 # En Dotter's nearby starts map
+	    self.totestra = 9 # En Dotter's nearby starts map
 
         #Number of tectonic plates
         self.hmNumberOfPlates = int(float(self.hmWidth * self.hmHeight) * 0.0016)
@@ -5796,15 +5796,15 @@ def getCustomMapOptionDescAt(argsList):
         if selectionID == 0:
             return "Random"
         elif selectionID == 1:
-            return "T1"
+            return "T5"
         if selectionID == 2:
-            return "T2"
+            return "T6"
         elif selectionID == 3:
-            return "T3"
+            return "T7"
 	elif selectionID == 4:
-	    return "T4"
+	    return "T8"
 	elif selectionID == 5:
-	    return "T5"
+	    return "T9"
     elif optionID == OPTION_IslandFactor:
         if selectionID == 0:
             return "Few (faster)"
