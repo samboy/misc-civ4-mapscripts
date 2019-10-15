d9261f030df342206951346e2e6129f7af5cf6e0
Derek Kulinski <d@kulinski.us>
2019-10-15 15:18:59 -0700

    cmp & xrange

--- TotestraRG32.py
+++ TotestraRG32.py
@@ -280,6 +280,19 @@ import sys
 import time
 import os
 
+from six.moves import xrange
+
+def cmp(x, y):
+    """
+    Replacement for built-in function cmp that was removed in Python 3
+
+    Compare the two objects x and y and return an integer according to
+    the outcome. The return value is negative if x < y, zero if x == y
+    and strictly positive if x > y.
+    """
+
+    return (x > y) - (x < y)
+
 # Options
 OPTION_MapSeed = 10
 OPTION_NewWorld = 0
