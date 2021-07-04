fbecf17151bc7062378a77dd0688e86852bdc544
Sam Trenholme <git@samiam.org>
2021-07-04 05:08:33 -0700

    crop.map.sh: Propose alternate crop

--- utilities/crop.map.sh
+++ utilities/crop.map.sh
@@ -6,5 +6,11 @@
 # x1/y1 Lower left corner; x2/y2 upper right corner.  Try to keep the map
 # a multiple of 4 squares in both dimensions.
 
+# One possible set of values is (40 18 125 77) which generate a somewhat 
+# bigger map with enough space around the main land to not allow players to
+# block sea routes once Dhows (Galleons) are in play.  Also, there is more
+# distant land to colonize with Dhows compared to the ArabianCaulixtla version
+# of the map.
+
 cat ArabianCaulixtlaFullWorld.CivWarlordsWBSave | \
 	./CropCiv4Map.py 43 13 118 72 > ArabianCaulixtlaCrop.CivWarlordsWBSave
