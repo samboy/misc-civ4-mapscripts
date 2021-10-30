273805265809ea9a5e1b676cf48d4c7f6752c1f0
Sam Trenholme <git@samiam.org>
2021-10-30 10:56:43 -0700

    find.good.land.sh: Take in to account desert on land
    We should have 40% or more desert to make it a good Arabian map.

--- old/find.good.land.sh
+++ old/find.good.land.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-cat tally | tr -d , | awk '
-	{land=$8;snow=$10;tundra=$12;min=$14;max=$16;
-	if(snow == 0 && land > 1400 && tundra < 10 && min > 20 && max < 78){
-		print $0}
+cat tally | tr -d , | tr -d '}' | awk '
+	{land=$8;snow=$10;tundra=$12;min=$14;max=$16;desert=$18;
+	 percent=desert/land
+	if(snow == 0 && 
+	   land > 1400 && 
+	   percent > .4 && 
+	   tundra < 10 && min > 20 && max < 78){
+		print $0 " desert " percent "%"}
 	}'
