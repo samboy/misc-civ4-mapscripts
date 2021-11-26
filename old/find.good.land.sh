6b4ed91358a0e7252e6d0fb56b4abdafffc8251e
Sam Trenholme <git@samiam.org>
2021-11-26 14:52:27 -0800

    find.good.land.sh: List smaller lands which meet criteria too

--- old/find.good.land.sh
+++ old/find.good.land.sh
@@ -7,7 +7,7 @@ cat tally | tr -d , | tr -d '}' | awk '
 	{land=$8;snow=$10;tundra=$12;min=$14;max=$16;desert=$18;
 	 percent=desert/land
 	if(snow == 0 && 
-	   land > 1400 && 
+	   #land > 1400 && 
 	   percent > .4 && 
 	   tundra < 10 && min > 20 && max < 78){
 		print $0 " desert " percent "%"}
