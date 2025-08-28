68f2931f21875a4afc492ed7bdcb9eac2fed3f87
Sam Trenholme <strenholme.usenet@gmail.com>
2025-08-28 00:47:52 -0600

    find.good.land.sh: Make tweakable

--- old/find.good.land.sh
+++ old/find.good.land.sh
@@ -3,12 +3,22 @@ if [ ! -e tally ] ; then
 	xzcat tally.txt.xz | tr -d '\015' > tally
 fi
 
-cat tally | tr -d , | tr -d '}' | awk '
+MAXTUNDRA=10
+if [ -n "$1" ] ; then
+  MAXTUNDRA="$1"
+fi
+FILENAME="tally"
+if [ -n "$2" ] ; then
+  FILENAME="$2"
+fi
+
+cat $FILENAME | tr -d , | tr -d '}' | awk '
 	{land=$8;snow=$10;tundra=$12;min=$14;max=$16;desert=$18;
 	 percent=desert/land
 	if(snow == 0 && 
 	   #land > 1400 && 
 	   percent > .4 && 
-	   tundra < 10 && min > 20 && max < 78){
+	   tundra < '$MAXTUNDRA' && 
+           min > 20 && max < 78){
 		print $0 " desert " percent "%"}
 	}'
