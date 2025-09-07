39326ffa0e13a0f9b44e822a14cf72c454ee594c
Sam Trenholme <strenholme.usenet@gmail.com>
2025-09-07 00:17:35 -0600

    Update .gitignore and find.good.land.sh for 144x96 batches

--- misc/find.good.land.sh
+++ misc/find.good.land.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-#SIZE=144x96
-SIZE=192x128
+# SIZE only affects where we look for tallies if tally does not exist
+SIZE=144x96
+#SIZE=192x128
 
 if [ ! -e tally ] ; then
 	xzcat tallies/${SIZE}/*txt.xz | tr -d '\015' > tally
