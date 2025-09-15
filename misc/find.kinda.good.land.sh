a28c480a69156264e286e85e511f0282a080472b
Sam Trenholme <strenholme.usenet@gmail.com>
2025-09-14 22:57:29 -0600

    Be less strict about the land we accept gives us many more possible maps

---
+++ misc/find.kinda.good.land.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# SIZE only affects where we look for tallies if tally does not exist
+SIZE=144x96
+#SIZE=192x128
+
+if [ ! -e tally ] ; then
+	xzcat tallies/${SIZE}/*txt.xz | tr -d '\015' > tally
+fi
+
+grep -F '@' tally | awk '{print $NF}' | awk -F, '{
+	seed=$1
+        for(a=2;a<NF;a++) {
+		islandSize = $a
+		sub(/@/,"",islandSize)
+		if($a ~ /@/ && (islandSize + 0) > 800) {
+			print "(" islandSize ", '\''" seed "'\'', " a - 1 "),"
+		}
+	}
+}'
