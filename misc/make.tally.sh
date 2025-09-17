83ba2c3aa5d20cd9295ad12438fa2029275c31e6
Sam Trenholme <strenholme.usenet@gmail.com>
2025-09-17 14:46:21 -0600

    make.tally.sh: Add command line argument for 192x128 maps
    make.tally.sh ${SEED} 192x128 makes 192x128 maps, or without
    size it makes 144x96 maps.

--- misc/make.tally.sh
+++ misc/make.tally.sh
@@ -2,14 +2,19 @@
 
 echo You may need to use ctrl+Z to stop this process
 
-#COMMAND=TotestraRG32.py    # 144x96 (Legacy RT### decimal integer seeds)
-COMMAND=TotestraRG32Hex.py  # 144x96
-#COMMAND=TotestraRG32Big.py # 192x128
-
 A="$1"
 
 if [ -z "$A" ] ; then
 	A=1
+fi
+
+#COMMAND=TotestraRG32.py    # 144x96 (Legacy RT### decimal integer seeds)
+COMMAND=TotestraRG32Hex.py  # 144x96
+#COMMAND=TotestraRG32Big.py # 192x128
+
+SIZE="$2"
+if [ "$SIZE" = "192x128" ] ; then
+  COMMAND=TotestraRG32Big.py # 192x128
 fi
 
 touch tally
