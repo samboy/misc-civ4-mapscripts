ca97523aa65af0639f8d1272fc6ee38ef2f5dbd0
Sam Trenholme <strenholme.usenet@gmail.com>
2025-10-25 18:08:19 -0600

    Better tool for finding interesting worlds

---
+++ misc/find.kinda.good.land.format2.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+TALLYFILE="$1"
+
+if [ -z "$1" ] ; then
+        TALLYFILE=tally
+fi
+
+# SIZE only affects where we look for tallies if tally does not exist
+SIZE=144x96
+#SIZE=192x128
+
+if [ ! -e $TALLYFILE ] ; then
+	xzcat tallies/${SIZE}/*txt.xz | tr -d '\015' > $TALLYFILE
+fi
+
+grep -F '@' $TALLYFILE | awk '{print $NF}' | awk -F, '{
+	seed=$1
+        for(a=2;a<NF;a++) {
+		islandSize = $a
+		sub(/@/,"",islandSize)
+		if($a ~ /@/ && (islandSize + 0) > 999) {
+			print a - 1 " " islandSize " " $0
+		}
+	}
+}' | sort -n
