75da85d293110dc397c10cc068ab25b2c1fea76d
Sam Trenholme <strenholme.usenet@gmail.com>
2025-11-26 10:07:16 -0600

    find.kinda.good.land.format2.sh: Make more friendly
    find.kinda.good.land.format2.sh now has --help option

--- misc/find.kinda.good.land.format2.sh
+++ misc/find.kinda.good.land.format2.sh
@@ -6,6 +6,21 @@ if [ -z "$1" ] ; then
         TALLYFILE=tally
 fi
 
+if [ $TALLYFILE = '--help' ] ; then
+	echo Usage: $0 {tallyfile} {x}
+	echo Both {tallyfile} and {x} are optional
+	echo if {x} is present, show by number of bigish islands
+	echo Example usage:
+	echo 'xzcat tallies/144x96/*xz > foo'
+	echo $0 'foo x'
+	echo When {x} is present, format is as follows:
+	echo '# of bigish islands; Index of Arabian land mass; '
+	echo 'Size of Arabian land mass; details'
+	echo '>>>Details are:'
+	echo 'Seed,size of each land mass/island on the map'
+	exit 0
+fi
+
 # SIZE only affects where we look for tallies if tally does not exist
 SIZE=144x96
 #SIZE=192x128
@@ -23,10 +38,14 @@ grep -F '@' $TALLYFILE | awk '{print $NF}' | awk -F, '{
 			print a - 1 " " islandSize " " $0
 		}
 	}
-}' | sort -n
+}' | sort -n > foo.$$
 
-# Uncomment to get sorted by biggish island count
-# | tr -d '@' | tr -d '-' | tr -d '+' | awk -F, '
-#{b=0;for(a=2;a<=NF;a++){if($a >= 242){b++}}print b " " $0}
-#' | sort -n
+if [ "$2" = "x" ] ; then
+	cat foo.$$ | tr -d '@' | tr -d '-' | tr -d '+' | awk -F, '
+		{b=0;for(a=2;a<=NF;a++){if($a >= 242){b++}}print b " " $0}
+	' | sort -n
+else
+	cat foo.$$
+fi
 
+rm foo.$$
